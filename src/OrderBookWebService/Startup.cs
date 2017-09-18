a20e348bea5693ad86af93a4d56b203c9f3ff39d
Adam Webb <adam.webb@perfectchannel.com>
2017-09-18 15:25:38 +0100

    Added CORS allowing any request to web service

--- src/OrderBookWebService/Startup.cs
+++ src/OrderBookWebService/Startup.cs
@@ -34,6 +34,11 @@ namespace OrderBookWebService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseCors(options => options
+                .AllowAnyMethod()
+                .AllowAnyOrigin()
+                .AllowAnyHeader());
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
