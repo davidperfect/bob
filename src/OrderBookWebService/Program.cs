d011d34024a13925fefbbad0560a35f930d40c99
Adam Webb <adam.webb@perfectchannel.com>
2017-09-18 14:52:09 +0100

    Changed API binding to 0.0.0.0:5000 instead of localhost

--- src/OrderBookWebService/Program.cs
+++ src/OrderBookWebService/Program.cs
@@ -17,6 +17,7 @@ namespace OrderBookWebService
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
+                .UseUrls("http://0.0.0.0:5000")    
                 .UseApplicationInsights()
                 .Build();
 
