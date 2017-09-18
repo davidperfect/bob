858c55a0cac1f49c82767eec19c7f80c17b1dec6
David.Perez <David.Perez@TS-WKSTN-018>
2017-09-18 20:43:29 +0100

    Added an orderbook page calling signalR, added manually 3rd party signalR script, added signalR npm package, created an orderbook hub

---
+++ src/OrderBookDemoUI/Views/OrderBook/Index.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrderBookDemoUI.Views.OrderBook
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
