ebfda0c49c041f8ece71d666fbe151ba6b0e57df
Adam Webb <adam.webb@perfectchannel.com>
2017-09-19 08:55:05 +0100

    Extracted out web service hub getter

--- src/OrderBookDemoUI/wwwroot/js/site.js
+++ src/OrderBookDemoUI/wwwroot/js/site.js
@@ -1,4 +1,7 @@
-﻿// Write your Javascript code.
-        function getWebServiceUri(path) {
+﻿        function getWebServiceUri(path) {
             return 'http://' + window.location.hostname + ':5000/api/' + path;
+        }
+
+        function getWebServiceHub(hubName) {
+            return 'http://' + window.location.hostname + ':5000/' + hubName;
         }
